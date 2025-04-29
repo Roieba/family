5db82ba61778157bc8f5862c7057d6176109b183
Roieba <roi.bena@gmail.com>
2025-04-29 15:22:40 +0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+
+importScripts('https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA2ik_QDKOvcE1vk-XumA9502ffRmiBm68",
+  authDomain: "vacationfamilychecklist.firebaseapp.com",
+  projectId: "vacationfamilychecklist",
+  storageBucket: "vacationfamilychecklist.appspot.com",
+  messagingSenderId: "652023210604",
+  appId: "1:652023210604:web:767527fac16f81686cee49"
+});
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: payload.notification.icon
+  });
+});
